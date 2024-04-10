359ffc09dd057fdc013667bfbc5515be0bb3b5db
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-10 14:41:26 +0200

    Update_DTOs

--- RiskAware.Server/DTOs/CommentDto.cs
+++ RiskAware.Server/DTOs/CommentDto.cs
@@ -2,5 +2,9 @@
 {
     public class CommentDto
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+        public string Author { get; set; }
     }
 }
