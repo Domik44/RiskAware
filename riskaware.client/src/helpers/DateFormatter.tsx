3d66a09785db211610ce24d787456d0e8aca33d7
David Drtil <david.drtil8@gmail.com>
2024-04-11 11:53:33 +0200

    Added cz date/datetime formatter

---
+++ riskaware.client/src/helpers/DateFormatter.tsx
@@ -0,0 +1,30 @@
+﻿// For better performance and zero padding datetime
+const DateFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("cs-CZ", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour12: false,
+  timeZone: 'Europe/Prague'
+});
+
+const DateTimeFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("cs-CZ", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+  timeZone: 'Europe/Prague'
+});
+
+// Format Date in czech format
+export function formatDate(date: Date) {
+  return date instanceof Date ? DateFormatter.format(date)
+    : DateFormatter.format(new Date(date));
+}
+
+// Format DateTime in czech format
+export function formatDateTime(date: Date) {
+  return date instanceof Date ? DateTimeFormatter.format(date)
+    : DateTimeFormatter.format(new Date(date));
+}
