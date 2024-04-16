70793f35598a338c719414d94e3b749a9d0c4dc2
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-04-16 16:03:11 +0200

    Update_DateFormatter

--- riskaware.client/src/helpers/DateFormatter.tsx
+++ riskaware.client/src/helpers/DateFormatter.tsx
@@ -17,6 +17,14 @@ const DateTimeFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("cs-CZ", 
   timeZone: 'Europe/Prague'
 });
 
+const DateInputFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("cs-CZ", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour12: false,
+  timeZone: 'Europe/Prague'
+});
+
 // Format Date in czech format
 export function formatDate(date: Date) {
   return date instanceof Date ? DateFormatter.format(date)
@@ -27,4 +35,16 @@ export function formatDate(date: Date) {
 export function formatDateTime(date: Date) {
   return date instanceof Date ? DateTimeFormatter.format(date)
     : DateTimeFormatter.format(new Date(date));
+}
+
+export function formatDateForInput(date: Date) {
+  const formattedDate = date instanceof Date ? DateInputFormatter.format(date)
+    : DateInputFormatter.format(new Date(date));
+
+  // Split the formatted date by space and reorder it to "yyyy-MM-dd"
+  const parts = formattedDate.split('.').map(part => part.trim());
+  const [day, month, year] = parts;
+
+  // Ensure that the parts are in "yyyy-MM-dd" format
+  return `${year}-${month}-${day}`;
 }
